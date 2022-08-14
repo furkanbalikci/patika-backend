a199f47895f501f937fd918ad1a16383f8454365
Furkan Balıkçı <46796424+furkanbalikci@users.noreply.github.com>
2022-08-14 15:59:47 +0300

    Create kdv-calculator.java
    KDV Calculator Program

---
+++ java101/kdv-calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static  void main(String[] args){
+
+        double amount,kdvRate,kdvAmount,total;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tutar giriniz");
+        amount = scanner.nextInt();
+        if ( amount > 1000.0) {
+            kdvRate = 0.08;
+        }else {
+            kdvRate = 0.18;
+        }
+        kdvAmount = amount * kdvRate;
+        total = kdvAmount+amount;
+        System.out.println("Girilen Tutar: "+ amount);
+        System.out.println("Kdv Oranı: "+ kdvRate);
+        System.out.println("Kdv Tutarı: " + kdvAmount);
+        System.out.println("Kdvli Tutar: "+total);
+
+    }
+
+}
