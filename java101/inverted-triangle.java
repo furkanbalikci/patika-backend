46fb3abb92d0459bf92cfc87eaac47f26547915c
Furkan Balıkçı <46796424+furkanbalikci@users.noreply.github.com>
2022-08-17 11:09:18 +0300

    Create inverted-triangle.java
    Inverted Triangle

---
+++ java101/inverted-triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.println("bir sayı giriniz: ");
+        n = scanner.nextInt();
+        for (int i = 1; i <= n ; i++) {
+            for (int j = n; j > (n - i); j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2*(n-i)-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
