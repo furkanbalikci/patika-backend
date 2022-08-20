4b82f772f9e4c4f6e8efa34c86c97895d534d9a1
Furkan Balıkçı <46796424+furkanbalikci@users.noreply.github.com>
2022-08-20 11:15:53 +0300

    Create harmonic-series-arrays.java
    Program to calculate harmonic mean of numbers in array

---
+++ java101/harmonic-series-arrays.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        double sum = 0;
+        for (int number : numbers) {
+            sum += 1 / number;
+        }
+        System.out.println(numbers.length/sum);
+
+    }
+}
