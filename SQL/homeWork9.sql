af318028fb806daff773629e50879a488dffed88
Furkan Balıkçı <46796424+furkanbalikci@users.noreply.github.com>
2022-10-18 20:13:51 +0300

    Create homeWork9.sql
    homeWork9

---
+++ SQL/homeWork9.sql
@@ -0,0 +1,11 @@
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select city, country from city
+inner join country on city.country_id = country.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select payment_id, first_name, last_name from payment
+inner join customer on payment.customer_id = customer.customer_id;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select rental_id, first_name, last_name from rental
+inner join customer on rental.customer_id = customer.customer_id;
