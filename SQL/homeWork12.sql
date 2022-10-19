e804e3da0b821a711116953574951cb1ab1398f1
Furkan Balıkçı <46796424+furkanbalikci@users.noreply.github.com>
2022-10-19 20:56:16 +0300

    Create homeWork12.sql
    homeWork12

---
+++ SQL/homeWork12.sql
@@ -0,0 +1,18 @@
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+select count(*) from film
+where length > (select avg(length) from film);
+
+--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+select count(*) from film
+where rental_rate = (select max(rental_rate) from film);
+
+--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+select count(*) from film
+where rental_rate = (select min(rental_rate) from film) and
+replacement_cost = (select min(replacement_cost) from film);
+
+--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+select * from customer
+where customer_id=(select customer_id FROM payment
+group by customer_id
+order by count(customer_id) DESC limit 1);
